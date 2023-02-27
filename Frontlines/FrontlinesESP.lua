1af07ede944d6c262af401448a5285a6a0e5eff3
Reno <101154856+RenoRenoRenoReno@users.noreply.github.com>
2023-02-27 18:14:18 -0500

    Create FrontlinesESP.lua

---
+++ Frontlines/FrontlinesESP.lua
@@ -0,0 +1,28 @@
+--< This code is old, and won't work very good, but here you go. >--
+_G.TeamColorEnabled = true
+_G.TeamColor = Color3.fromRGB("ColorHere")
+_G.EnemyColor = 'default'
+
+
+while task.wait(0.55)  do --< For Updating Better >--
+for _,User in pairs(game.workspace:GetChildren()) do
+	   if (User:FindFirstChild("TPVAccessoryMagpack2L")) then
+	   local highlight = Instance.new("Highlight", User)
+	   if (_G.EnemyColor == 'default' or _G.EnemyColor == 'Default' ) then
+	   	      print("Not changing ENEMY COLOR ESP")
+	      else
+	          highlight.FillColor = _G.EnemyColor	
+	          print("Changed ESP Color")
+	   end
+	   if (User:FindFirstChild("friendly_marker")) then
+	   	  if (User['friendly_marker']:FindFirstChild('name_label')) then
+	   	  	 if (_G.TeamColorEnabled == false) then
+	   	  	    print("Found teammate, not changing ESP Color")
+		     end
+	   	     print("Found friendly user. Changing ESP color.")
+	   	     highlight.FillColor = _G.TeamColor
+		  end
+	   end
+	end
+end	
+end
